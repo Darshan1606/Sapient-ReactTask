124dfbeec666b8b95d8d8d74c7a891ac73d6373c
Darshan1606 <darshantarsariya17@gmail.com>
2023-01-19 17:55:53 +0530

    nav toggle icon changed success

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,24 +11,33 @@ const Navbar = () => {
 
     return (
         <div className="navbarclass">
-            <button 
-            onClick={() => setIsNavExpanded(!isNavExpanded)}
-            className="hamburger" 
-            aria-controls="primary-navigation" 
-            aria-expanded="true"
+            <button
+                onClick={() => setIsNavExpanded(!isNavExpanded)}
+                className="hamburger"
+                aria-controls="primary-navigation"
+                aria-expanded="true"
             >
+                <svg width="40" height="17" xmlns="http://www.w3.org/2000/svg"><g fill="#00001A" fill-rule="evenodd"><path d="M0 0h40v3H0zM0 7h40v3H0zM0 14h40v3H0z"/><path d="M0 0h40v3H0z"/></g></svg>
             </button>
             < nav >
                 <img className="logo" alt="logo" src={image} />
-               
 
-                <div className={isNavExpanded ? " nav-menu" : "nav-expand" } >
+
+                <div className={isNavExpanded ? "nav-expand " : "nav-menu"} >
                     <ul data-visible="false" class="primary-navigation flex">
                         <li className="active"><a href="#" aria-hidden="true">Home</a></li>
                         <li><a href="#">New</a></li>
                         <li><a href="#">Popular</a></li>
                         <li><a href="#">Trending</a></li>
                         <li><a href="#">Catagories</a></li>
+                        <button
+                            onClick={() => setIsNavExpanded(!isNavExpanded)}
+                            className="hamburger"
+                            aria-controls="primary-navigation"
+                            aria-expanded="true"
+                        >
+                            <svg width="32" height="31" xmlns="http://www.w3.org/2000/svg"><g fill="#00001A" fill-rule="evenodd"><path d="m2.919.297 28.284 28.284-2.122 2.122L.797 2.419z"/><path d="M.797 28.581 29.081.297l2.122 2.122L2.919 30.703z"/></g></svg>
+                        </button>
                     </ul>
                 </div>
             </nav>
